f1bce4077979b9871d54b7fbde4bfb7d53ee6fb4
fmings <mingsfelicia@gmail.com>
2024-04-08 15:29:47 -0500

    finished file setup

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import User from '../components/User';
+import Signout from '../components/Signout';
 
 export default function Profile() {
-  return <User />;
+  return (
+    <>
+      <User />;
+      <Signout />;
+    </>
+  );
 }
